18c2e18ac04cddee94f3642760842218380bc5b0
Erik Nascimento <erik@2eg.com.br>
2018-11-15 15:09:04 -0200

    chrome options

--- crawler.py
+++ crawler.py
@@ -23,11 +23,11 @@ def api_name():
 
         options = webdriver.ChromeOptions()
         # options.add_argument("headless")
-        options.add_argument("start-maximized"); // open Browser in maximized mode
-        options.add_argument("disable-infobars"); // disabling infobars
-        options.add_argument("--disable-extensions"); // disabling extensions
+        options.add_argument("start-maximized") # open Browser in maximized mode
+        options.add_argument("disable-infobars") # disabling infobars
+        options.add_argument("--disable-extensions") # disabling extensions
         options.add_argument("--no-sandbox")
-        options.add_argument("--disable-dev-shm-usage"); // overcome limited resource problems
+        options.add_argument("--disable-dev-shm-usage") # overcome limited resource problems
         driver = webdriver.Chrome()
         driver.get('https://melhorplano.net/planos-de-celular/resultado?n={}&c=vivo%2Coi%2Ctim%2Cclaro%2Cnextel&t=controle%2Cpost&i0=&m0=&ps=vivo%2Coi%2Ctim%2Cclaro%2Cnextel&mn=1&p=mobile'.format(lines))
         p_element = driver.find_elements_by_class_name(name='result-card')
