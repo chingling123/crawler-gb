ef3c2ebe181b396ff43676a7b0355210df136237
Erik Nascimento <erik@2eg.com.br>
2018-11-15 13:39:01 -0200

    path

--- crawler.py
+++ crawler.py
@@ -21,9 +21,10 @@ def api_name():
         gbRequest = float(request.args['gb'])
         priceRequest = request.args['price']
 
+        chrome_exec_shim = os.environ.get("GOOGLE_CHROME_BIN", "chromedriver")
         options = webdriver.ChromeOptions()
         options.add_argument("headless")
-        driver = webdriver.Chrome()
+        driver = webdriver.Chrome(executable_path=chrome_exec_shim)
         driver.get('https://melhorplano.net/planos-de-celular/resultado?n={}&c=vivo%2Coi%2Ctim%2Cclaro%2Cnextel&t=controle%2Cpost&i0=&m0=&ps=vivo%2Coi%2Ctim%2Cclaro%2Cnextel&mn=1&p=mobile'.format(lines))
         p_element = driver.find_elements_by_class_name(name='result-card')
         planos = []
