b5ec2c992584a243826ee31a16ac81a89f1a7711
Erik Nascimento <erik@2eg.com.br>
2018-11-15 16:37:55 -0200

    new class

---
+++ returntogb.py
@@ -0,0 +1,9 @@
+class ReturnToGB:
+    def __init__(self, planos):
+        self.payload = planos
+        self.name = "judith:event"
+        self.version = 1
+        self.id = "e577b9d8-5b39-4aa6-bd74-418ec2f74174"
+        self.flowId = "49689dcf-80c4-45a6-9f82-61a240e49a5c"
+        
+        
